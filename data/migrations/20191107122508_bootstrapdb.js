b4c54a46455989b67119f3c51a7164f2aaad509a
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-07 12:57:38 -0600

    migrated?

--- data/migrations/20191107122508_bootstrapdb.js
+++ data/migrations/20191107122508_bootstrapdb.js
@@ -1,37 +1,51 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('species', tbl => {
-      tbl.increments();
+    return knex.schema
+      .createTable('species', tbl => {
+        // the type of the Primary Key is: integer without negative values, also called unsigned
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+      })
+      .createTable('animals', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+        // define our Foreign Key
+        tbl
+          .integer('species_id')
+          .unsigned()
+          .references('id')
+          .inTable('species')
+          .onDelete('RESTRICT') // about deleting the record from the primary key table. Could be 'RESTRICT', 'NO ACTION', 'SET NULL'
+          .onUpdate('CASCADE'); // about changing the value of the primary key table.
+        // we have bears and a few animals that are bears.
+      })
+      .createTable('zoos', tbl=>{
+          tbl.increments()
+          tbl.string('name', 255).notNullable();
+          tbl.string('address', 255).notNullable()
+      })
+      .createTable('animal_zoo', tbl=>{
+          tbl.increments();
+          tbl
+          .integer('zoo_id')
+          .unsigned()
+          .references('id')
+          .inTable('species')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+          tbl
+          .integer('animal_id')
+          .unsigned()
+          .references('id')
+          .inTable('species')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      })
 
-      tbl.string('name',255).notNullable();
-  })
-  .createTable('animals', tbl=>{
-      tbl.inrements();
-
-      tbl.string('name',255).notNullable();
-      
-    //define our foriegn key
-      tbl
-      .integer('species_id')
-      .unsigned()
-      .references('id')
-      .inTable('species')
-      .onDelete('RESTRICT')//about deleting the record from the primary key table. Could be 'RESTRICT','NO ACTION','SET NULL'
-      .onUpdate('CASCADE')//about hanging the value of the primary key.
-
-    // we have bears and a few animals that are bears
-
-  })
-  .createTable('animal_zoos', tbl =>{
-      tbl.increments();
-      tbl.string('zoo_id',255).notNullable();
-      tbl.string('animal_id',255).notNullable();
-      tbl.string()
-  .ceateTable('')
-
-};
-
-exports.down = function(knex) {
-  
-};
-// knex command not found = no knex on global run npm i -g knex
+  };
+  exports.down = function(knex) {
+      return knex.schema
+      .dropTableIfExists('species')
+      .dropTableIfExists('animals')
+      .dropTableIfExists('zoos')
+      .dropTableIfExists('animal_zoo')
+  };
